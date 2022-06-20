5978c2412bf161b57aad0d3b172bd7bbb5f12975
pedr0fontoura <pedrofontoura@id.uff.br>
2022-06-19 23:34:59 -0300

    fix: remove type guard left-over

--- utils/typeGuards.ts
+++
@@ -1,8 +0,0 @@
-import { IPostData } from '@/types/mdx';
-
-export function isPostData(variableToCheck: any): variableToCheck is IPostData {
-  return (
-    !!(variableToCheck as IPostData).title &&
-    !!(variableToCheck as IPostData).publishedAt
-  );
-}
